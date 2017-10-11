2bce3676d42003772c2ea26dcda45ffea48c9ca1
Unknown <jsandrade1998@gmail.com>
2017-10-11 09:31:38 +0200

    Primmer Commit

---
+++ app/src/main/java/com/example/jstalin/aceptapolitica/AceptaPolitica.java
@@ -0,0 +1,13 @@
+package com.example.jstalin.aceptapolitica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AceptaPolitica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acepta_politica);
+    }
+}
